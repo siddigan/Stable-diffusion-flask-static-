3e1f9d4a4889db78826a1b03665a1788d244fce7
[siddigan] <[2001siddi@gmail.com]>
2023-11-09 13:38:42 +0530

    minor

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from flask_ngrok import run_with_ngrok
 from flask import Flask, render_template, request
-
 import torch
 from diffusers import StableDiffusionPipeline
-
 import base64
 from io import BytesIO
 
-# Load model
+
+# Load model:
 pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
 pipe.to("cuda")
 
