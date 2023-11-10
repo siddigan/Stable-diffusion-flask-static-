06151afd13b6b638cf99bc20d94b81134bda70b9
[siddigan] <[2001siddi@gmail.com]>
2023-11-10 23:09:51 +0530

    minor2

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ import base64
 from io import BytesIO
 
 
-# Load model:
 pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", revision="fp16", torch_dtype=torch.float16)
 pipe.to("cuda")
 
-# Start flask app and set to ngrok
 app = Flask(__name__)
 run_with_ngrok(app)
 
